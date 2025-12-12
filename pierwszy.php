d7edcf17166b6522c7340154d4bfb7192b085fa1
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-12-12 10:51:21 +0100

    php1

---
+++ pierwszy.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php   //początek skryptu PHP
+        echo "Witaj w świecie PHP!";
+    
+            /*
+            Komentarz wieloliniowy
+            Drugi wiersz komentarza
+            Trzeci wiersz komentarza
+            */
+ 
+ ?> <!--koniec skryptu PHP-->
+    
+</body>
+</html>
