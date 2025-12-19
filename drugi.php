202bcfd9e09169a526ee0c9acdac399fed454013
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-12-19 08:10:13 +0100

    php2

---
+++ drugi.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instrukcje warunkowe i pętle</title>
+</head>
+<body>
+    <h1>Instrukcje warunkowe i pętle</h1>
+    <p>W tym pliku znajdziesz przykłady instrukcji warunkowych i pętli w PHP.</p>
+    <?php
+        $liczba = 6;
+        // Instrukcja warunkowa if
+        if ($liczba > 5) {
+            echo "<p>Liczba $liczba jest większa niż 5.</p>";
+        } 
+
+        
+        ?>
+</body>
+</html>
