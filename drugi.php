b3fb1e80c1f9ba252b859b4cb83b585bab90b06f
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-12-19 08:15:21 +0100

    php2

--- drugi.php
+++ drugi.php
@@ -14,8 +14,31 @@
         if ($liczba > 5) {
             echo "<p>Liczba $liczba jest większa niż 5.</p>";
         } 
+        // Instrukcja warunkowa if-else
+        $liczba1 = 8;
 
+        if($liczba1 > 10) {
+            echo "<p>Liczba $liczba1 jest większa niż 10.</p>";
+        }
         
+        else {
+            echo "<p>Liczba $liczba1 jest mniejsza od 10.</p>";
+        }
+
+        $liczba2 = 122;
+        // Instrukcja warunkowa if-elseif-else
+        if($liczba2 < 15) {
+            echo "<p>Liczba $liczba2 jest mniejsza niż 15.</p>";
+        }
+        elseif($liczba2 == 15 ) {
+            echo "<p>Liczba $liczba2 jest równa 15.</p>";
+        }
+        else {
+            echo "<p>Liczba $liczba2 jest większa niż 15.</p>";
+        }
+
+
+
         ?>
 </body>
 </html>
