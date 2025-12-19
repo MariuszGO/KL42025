d656de4343ed04bbdc9257428d22a10f5606120b
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-12-19 09:00:32 +0100

    php2

--- drugi.php
+++ drugi.php
@@ -36,6 +36,46 @@
         else {
             echo "<p>Liczba $liczba2 jest większa niż 15.</p>";
         }
+        //Przykłady z operatorami logicznymi
+        //liczba większa od 10 i mniejsza od 20
+        $liczba3 = 15;
+        if($liczba3 > 10 && $liczba3 < 20) {
+            echo "<p>Liczba $liczba3 jest większa od 10 i mniejsza od 20.</p>";
+        }
+
+        //Instrukcja switch
+        $dzien_tygodnia = 6;
+        switch($dzien_tygodnia) {
+            case 1:
+                echo "<p>Dzisiaj jest poniedziałek.</p>";
+                echo "<p>To początek tygodnia.</p>";
+                echo "<p>Miłego dnia!</p>";
+                echo "Każdy lubi poniedziałki!";
+                echo "<hr>";
+                break;
+            case 2:
+                echo "<p>Dzisiaj jest wtorek.</p>";
+                break;
+            case 3:
+                echo "<p>Dzisiaj jest środa.</p>";
+                break;
+            case 4:
+                echo "<p>Dzisiaj jest czwartek.</p>";
+                break;
+            case 5:
+                echo "<p>Dzisiaj jest piątek.</p>";
+                break;
+            case 6:
+                echo "<p>Dzisiaj jest sobota.</p>";
+                break;
+            case 7:
+                echo "<p>Dzisiaj jest niedziela.</p>";
+                break;
+            default:
+                echo "<p>Podałeś wartość spoza zakresu.</p>";
+        }
+
+
 
 
 
