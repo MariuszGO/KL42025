c76fe6cb2bea46ee95cbc031ab7b39b40adc563e
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-01-23 08:28:19 +0100

    php4

---
+++ piaty.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pętle</title>
+</head>
+<body>
+    
+<?php
+
+$i = 0;
+echo "<H1> PĘTLA DO WHILE </H1>";
+do{
+    echo "<p>$i</p>";
+    $i=$i+2;     // $i+=2;
+}while($i<=100);
+
+echo "<H1> PĘTLA WHILE </H1>";
+while($i<=100){
+
+
+
+}
+
+
+
+
+
+?>
+
+
+</body>
+</html>
