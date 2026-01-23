2a98a5bc14f108185ed789e75979610163d41cd0
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-01-23 09:06:41 +0100

    php4

--- piaty.php
+++ piaty.php
@@ -23,12 +23,29 @@ echo "<H2>I ma wartosc  $i</H2>";
 echo "<H1> PĘTLA WHILE </H1>";
 while($i<=100){
 
-    if($i % 2 == 0){
+    if($i % 2 == 0){    //sprawdzanie czy liczba jest parzysta
+                        //jeżeli reszta z dzielenia całkowitego jest równa 0 to liczba
+                        //jest parzysta i zostanie wyświetlona w przeciwnym razie instrukcja
+                        //wewnątrz if zostanie pominięta
+                        //warunek czytamy "i modulo 2 czy jest równe 0"
                     echo "<p>$i</p>";
                     }
     $i++;   // altrernatywny zapis $i=$i+1   lub $i+=1;
 }
 
+echo "<H2>I ma wartosc  $i</H2>"; //jest równe 101
+echo "<H1> PĘTLA FOR </H1>";
+
+for($j=0;$j<$i;$j+=2){
+    echo "<p>$j</p>";
+}
+
+$tablica = array(124,466,344,233,894,343,111,222,567);
+
+echo "<H1> PĘTLA FOREACH (do obsługi tablic) </H1>";
+foreach($tablica as $klucz){
+    echo "<p>$klucz</p>";
+}
 
 
 
