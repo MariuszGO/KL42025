4dcc8e35e27828792e9ee9883122e244371f8891
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-02-20 11:25:12 +0100

    bazy2

--- bazy.php
+++ bazy.php
@@ -1,50 +1,63 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pl-PL">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BAZY</title>
 </head>
-
 <body>
-    <form action="bazy.php" method="post">
-        <label for="marka">Marka</label>
-        <input type="text" name="marka" id="marka"><br>
+<form action="bazy.php" method="post">
+<label for="marka">Marka</label>
+<input type="text" name="marka" id="marka"><br> 
+<label for="model">Model</label>
+<input type="text" name="model" id="model"><br>
+<label for="cena">Cena</label>
+<input type="number" name="cena" id="cena"><br>
+<input type="submit" value="Wyślij">
+</form>
 
-        <label for="model">Model</label>
-        <input type="text" name="model" id="model"><br>
+<?php
 
-        <label for="cena">Cena</label>
-        <input type="number" name="cena" id="cena"><br>
+ $polaczenie = mysqli_connect("localhost","root","","samochody");
+   
 
-        <input type="submit" value="Wyślij">
-    </form>
+$marka = $_POST["marka"];
+$model = $_POST["model"];
+$cena = $_POST["cena"];
 
-    <?php
-    $polaczenie = mysqli_connect("localhost", "root", "", "samochody");
+//echo $marka." ".$model." ".$cena;
 
-    $marka = $_POST["marka"];
-    $model = $_POST["model"];
-    $cena = $_POST["cena"];
 
-    // echo $marka . " " . $model . " " . $cena;
+
+$test = false;
+
+$zapytanie1 = "Select * from pojazdy";
+$zmienna1 = mysqli_query($polaczenie,$zapytanie1);
+while ($wiersz = mysqli_fetch_array($zmienna1)) {
+    echo $wiersz['marka']." ".$wiersz['model']." ".$wiersz['cena']."<br>";
+
+    if($wiersz['marka'] == $_POST["marka"] && $wiersz['model'] == $_POST["model"] && $wiersz['cena'] == $_POST["cena"]){
+        $test = true;
+    } 
+
+}
+
+if($test == true) {
+echo ' ';
+}
+else {
     if (isset($marka) && isset($model) && isset($cena)) {
-        $zapytanie = "Insert into pojazdy values (NULL,'$marka','$model',$cena)";
-        $zmienna = mysqli_query($polaczenie, $zapytanie);
-    } else {
-        echo "brak danych";
-    }
+    $zapytanie = "Insert into pojazdy values (NULL,'$marka','$model',$cena)";
+    $zmienna = mysqli_query($polaczenie,$zapytanie);
+}
+else {
+    echo "brak danych";
+}
+}   
 
-    $zapytanie1 = "Select * from pojazdy";
-    $zmienna1 = mysqli_query($polaczenie, $zapytanie1);
 
-    while ($wiersz = mysqli_fetch_array($zmienna1)) {
-        echo $wiersz['marka'] . " " . $wiersz['model'] . " " . $wiersz['cena'] . "<br>";
-    }
-
-    mysqli_close($polaczenie);
-    ?>
+mysqli_close($polaczenie);
+?>
+    
 </body>
-
 </html>
